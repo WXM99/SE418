e231e05343521a0e80acc24862241de7f646ace2
WeiXM <>
2019-03-17 16:32:08 +0800

    Update HelloWorld.vue

--- VUE/src/components/HelloWorld.vue
+++ VUE/src/components/HelloWorld.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="hello">
     <Row>
-      <Col span="8" offset="8">
-        <Card style="margin-top: 100px">
+      <Col :md="{span: 12, offset: 6}" :lg="{span: 8, offset: 8}" :xs="{span:22, offset: 1}" :sm="{span: 22, offset: 1}" >
+        <Card style="margin-top: 100px" class="wd-card">
           <img src="../assets/wordLadder.png">
           <Row>
-    <Input v-model="first_word" @on-change="validator1" @on-blur="check">
+    <Input v-model="first_word" @on-change="validator1" @on-blur="check" style="margin-top: 40px">
       <span slot="prepend">&nbsp;&nbsp;1st word</span>
     </Input>
           </Row>
@@ -22,11 +22,11 @@
             <span v-if="!valid2 && !input2">invalid input</span>
             <span v-if="valid2 && !input2" style="color: #65b55c">okay!</span>
           </Row>
-    <Button type="primary" v-if="!(this.valid)" disabled @click="generate">生成</Button>
-    <Button type="primary" v-if="this.valid" @click="generate">生成</Button>
+    <Button type="primary" v-if="!(this.valid)" disabled @click="generate" style="font-size: 20px">Generate</Button>
+    <Button type="primary" v-if="this.valid" @click="generate" style="font-size: 20px">Generate</Button>
           <div v-if="dispaly" style="margin-top: 20px">
             <div v-if="this.chain === null">No ladder between {{this.first_word}}&{{this.second_word}}</div>
-            <div v-else>{{this.chain}}</div>
+            <div v-else style="color: #fff; font-weight: bold">{{this.chain}}</div>
           </div>
         </Card>
       </Col>
@@ -121,5 +121,15 @@ a {
 }
   .hello{
     height: 100%;
+  }
+  .wd-card{
+    border: 0px;
+    background: rgba(237, 233, 255, 0.1);
+    -webkit-backdrop-filter: saturate(180%) blur(5px);
+    height: 400px;
+    border-radius: 30px;
+    margin-top: 200px;
+    box-shadow: 0px 5px 20px rgb(27, 31, 38);
+    padding: 20px;
   }
 </style>
